4a61fb41ef435809eaf92d46247919388b0f8971
Saeedullah15 <saeedullahazim65@gmail.com>
2024-11-08 04:32:43 +0600

    image problem fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,10 @@ import { IoChatbubblesOutline } from "react-icons/io5";
 import { FaRegCalendarDays } from "react-icons/fa6";
 import { FaBuffer } from "react-icons/fa6";
 import { LuClipboardCheck } from "react-icons/lu";
+import pp1 from "../assets/pp1.png";
+import pp2 from "../assets/pp2.png";
+import pp3 from "../assets/pp3.png";
+import pp4 from "../assets/pp4.png";
 
 const Card = () => {
     const [modal, setModal] = useState(false);
@@ -29,11 +33,11 @@ const Card = () => {
             {/* first row */}
             <div className="flex justify-between items-center">
                 <div className="flex items-center gap-1">
-                    <img className="w-7 h-7 rounded-full" src="/src/assets/pp1.png" alt="" />
+                    <img className="w-7 h-7 rounded-full" src={pp1} alt="" />
                     <h3 className="font-semibold text-sm">Alex Sandro</h3>
                 </div>
                 <div className="flex items-center gap-1">
-                    <img className="w-7 h-7 rounded-full" src="/src/assets/pp2.png" alt="" />
+                    <img className="w-7 h-7 rounded-full" src={pp2} alt="" />
                     <h3 className="font-semibold text-sm">Robert John</h3>
                 </div>
             </div>
@@ -53,8 +57,8 @@ const Card = () => {
             {/* third row */}
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-3">
-                    <img className="w-7 h-7 rounded-full" src="/src/assets/pp3.png" alt="" />
-                    <img className="w-7 h-7 rounded-full" src="/src/assets/pp4.png" alt="" />
+                    <img className="w-7 h-7 rounded-full" src={pp3} alt="" />
+                    <img className="w-7 h-7 rounded-full" src={pp4} alt="" />
                     <p className="w-8 h-8 rounded-full bg-gray-100 p-1 font-semibold text-sm">12+</p>
                     <div className="flex items-center space-x-1">
                         <IoChatbubblesOutline /> <span className="font-semibold text-sm">15</span>
